33c30774ef10f0033fd81993cfcafeecdc291416
Rakshith <67837463+Rakhulant@users.noreply.github.com>
2022-07-24 16:37:06 +0530

    pdf

--- lib/applyleave.dart
+++ lib/applyleave.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:intl/intl.dart';
-import 'package:file_picker/file_picker.dart';
+//import 'package:file_picker/file_picker.dart';
 
 class ApplyLeave extends StatefulWidget {
   const ApplyLeave({Key? key}) : super(key: key);
@@ -55,151 +55,148 @@ class _ApplyLeaveState extends State<ApplyLeave> {
         ),
       ),
       body: SafeArea(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text('No of leaves left : 69 '),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                children: [
-                  Text('Leave type : '),
-                  DropdownButton(
-                    //isExpanded: true, // here need to change
-                    value: dropdownValue,
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        dropdownValue = newValue!;
-                      });
-                    },
-                    items: <String>[
-                      'SL',
-                      'CL',
-                      'ML',
-                    ].map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Padding(
-                          padding: const EdgeInsets.fromLTRB(0, 0, 50, 0),
-                          child: Text(value),
-                        ),
-                      );
-                    }).toList(),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: TextField(
-                controller: dateinput, //editing controller of this TextField
-                decoration: InputDecoration(
-                    icon: Icon(Icons.calendar_today), //icon of text field
-                    labelText: "From" //label text of field
-                    ),
-                readOnly:
-                    true, //set it true, so that user will not able to edit text
-                onTap: () async {
-                  DateTime? pickedDate = await showDatePicker(
-                      context: context,
-                      initialDate: DateTime.now(),
-                      firstDate: DateTime(
-                          2000), //DateTime.now() - not to allow to choose before today.
-                      lastDate: DateTime(2101));
-
-                  if (pickedDate != null) {
-                    print(
-                        pickedDate); //pickedDate output format => 2021-03-10 00:00:00.000
-                    String formattedDate =
-                        DateFormat('yyyy-MM-dd').format(pickedDate);
-                    print(
-                        formattedDate); //formatted date output using intl package =>  2021-03-16
-                    //you can implement different kind of Date Format here according to your requirement
-
+        child: Column(children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text('No of leaves left : 69 '),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                Text('Leave type : '),
+                DropdownButton(
+                  //isExpanded: true, // here need to change
+                  value: dropdownValue,
+                  onChanged: (String? newValue) {
                     setState(() {
-                      dateinput.text =
-                          formattedDate; //set output date to TextField value.
+                      dropdownValue = newValue!;
                     });
-                  } else {
-                    print("Date is not selected");
-                  }
-                },
-              ),
-            ),
-            Padding(padding: EdgeInsets.all(10)),
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: TextField(
-                controller: dateinputto, //editing controller of this TextField
-                decoration: InputDecoration(
-                    icon: Icon(Icons.calendar_today), //icon of text field
-                    labelText: "To" //label text of field
-                    ),
-                readOnly:
-                    true, //set it true, so that user will not able to edit text
-                onTap: () async {
-                  DateTime? pickedDate = await showDatePicker(
-                      context: context,
-                      initialDate: DateTime.now(),
-                      firstDate: DateTime(
-                          2000), //DateTime.now() - not to allow to choose before today.
-                      lastDate: DateTime(2101));
-
-                  if (pickedDate != null) {
-                    print(
-                        pickedDate); //pickedDate output format => 2021-03-10 00:00:00.000
-                    String formattedDate =
-                        DateFormat('yyyy-MM-dd').format(pickedDate);
-                    print(
-                        formattedDate); //formatted date output using intl package =>  2021-03-16
-                    //you can implement different kind of Date Format here according to your requirement
-
-                    setState(() {
-                      dateinputto.text =
-                          formattedDate; //set output date to TextField value.
-                    });
-                  } else {
-                    print("Date is not selected");
-                  }
-                },
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Row(
-                children: [
-                  Expanded(child: SizedBox()),
-                  ElevatedButton(
-                    onPressed: () async {
-                      FilePickerResult? result =
-                          await FilePicker.platform.pickFiles();
-                      if (result == null) {
-                        print("No file selected");
-                      } else {
-                        print(result.files.single.name);
-                      }
-                    },
-                    child: Text("Upload files"),
-                  ),
-                ],
-              ),
-            ),
-            Padding(padding: EdgeInsets.all(30)),
-            ElevatedButton(
-                    style: raisedButtonStyle,
-                    onPressed: () {
-                    },
-                    child: Text(
-                      'APPLY',
-                      style: TextStyle(
-                        color: Colors.white,
+                  },
+                  items: <String>[
+                    'SL',
+                    'CL',
+                    'ML',
+                  ].map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 0, 50, 0),
+                        child: Text(value),
                       ),
-                    ),
+                    );
+                  }).toList(),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextField(
+              controller: dateinput, //editing controller of this TextField
+              decoration: InputDecoration(
+                  icon: Icon(Icons.calendar_today), //icon of text field
+                  labelText: "From" //label text of field
                   ),
-          ]
-        ),
+              readOnly:
+                  true, //set it true, so that user will not able to edit text
+              onTap: () async {
+                DateTime? pickedDate = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(
+                        2000), //DateTime.now() - not to allow to choose before today.
+                    lastDate: DateTime(2101));
+
+                if (pickedDate != null) {
+                  print(
+                      pickedDate); //pickedDate output format => 2021-03-10 00:00:00.000
+                  String formattedDate =
+                      DateFormat('yyyy-MM-dd').format(pickedDate);
+                  print(
+                      formattedDate); //formatted date output using intl package =>  2021-03-16
+                  //you can implement different kind of Date Format here according to your requirement
+
+                  setState(() {
+                    dateinput.text =
+                        formattedDate; //set output date to TextField value.
+                  });
+                } else {
+                  print("Date is not selected");
+                }
+              },
+            ),
+          ),
+          Padding(padding: EdgeInsets.all(10)),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextField(
+              controller: dateinputto, //editing controller of this TextField
+              decoration: InputDecoration(
+                  icon: Icon(Icons.calendar_today), //icon of text field
+                  labelText: "To" //label text of field
+                  ),
+              readOnly:
+                  true, //set it true, so that user will not able to edit text
+              onTap: () async {
+                DateTime? pickedDate = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(
+                        2000), //DateTime.now() - not to allow to choose before today.
+                    lastDate: DateTime(2101));
+
+                if (pickedDate != null) {
+                  print(
+                      pickedDate); //pickedDate output format => 2021-03-10 00:00:00.000
+                  String formattedDate =
+                      DateFormat('yyyy-MM-dd').format(pickedDate);
+                  print(
+                      formattedDate); //formatted date output using intl package =>  2021-03-16
+                  //you can implement different kind of Date Format here according to your requirement
+
+                  setState(() {
+                    dateinputto.text =
+                        formattedDate; //set output date to TextField value.
+                  });
+                } else {
+                  print("Date is not selected");
+                }
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              children: [
+                Expanded(child: SizedBox()),
+                ElevatedButton(
+                  onPressed: () async {
+                    // FilePickerResult? result =
+                    //     await FilePicker.platform.pickFiles();
+                    // if (result == null) {
+                    //   print("No file selected");
+                    // } else {
+                    //   print(result.files.single.name);
+                    // }
+                  },
+                  child: Text("Upload files"),
+                ),
+              ],
+            ),
+          ),
+          Padding(padding: EdgeInsets.all(30)),
+          ElevatedButton(
+            style: raisedButtonStyle,
+            onPressed: () {},
+            child: Text(
+              'APPLY',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ]),
       ),
     );
   }
--- lib/payslip.dart
+++ lib/payslip.dart
@@ -1,4 +1,14 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:path_provider/path_provider.dart';
+import 'package:pspdfkit_flutter/src/main.dart';
 
 class payslip extends StatefulWidget {
   const payslip({Key? key}) : super(key: key);
@@ -8,8 +18,66 @@ class payslip extends StatefulWidget {
 }
 
 class _payslipState extends State<payslip> {
-  final List<String> month = <String>['sep', 'oct', 'dec'];
-  String dropdownValue = '2022';
+  var fileName = '/pspdfkit-flutter-quickstart-guide.pdf';
+  var year;
+  var month;
+  var storage = FlutterSecureStorage();
+  bool trydown = false;
+  var reason = '';
+
+  Future fetchAlbum(BuildContext context) async {
+    bool trylog = false;
+    var session = await storage.read(key: 'cookie');
+    Map<String, dynamic> jsonMap = {
+      "month": month,
+      "year": year,
+    };
+    print(jsonMap);
+
+    final uri = Uri.parse(
+        'https://hrmsprime.com/my_services_api/partner/get_payslip_id');
+
+    final response =
+        await http.get(uri.replace(queryParameters: jsonMap), headers: {
+      "Cookie": session.toString(),
+    });
+    print(response.body);
+    if (response.statusCode == 200) {
+      var det = jsonDecode(response.body);
+
+      if (det['status'] == 'success') {
+        var url = det['url'];
+        setState(() {
+          reason = 'Downloading...';
+          trydown = true;
+        });
+        print(url);
+        var res = await http.get(Uri.parse(url), headers: {
+          "Cookie": session.toString(),
+          "Accept": "*/*",
+          "Accept-Encoding": "gzip, deflate, br",
+          "Connection": "keep-alive"
+        });
+        print(res.body);
+        // var res = await Dio().post(url,
+        //     options: Options(
+        //         headers: {"Cookie": session.toString()},
+        //         responseType: ResponseType.bytes));
+        // log(res.data.toString());
+        // var tempDir = await getTemporaryDirectory();
+        // var file = File(tempDir.path + fileName).openSync(mode: FileMode.write);
+        // file.writeFromSync(res.data);
+        // await file.close();
+        // await Pspdfkit.present(tempDir.path + fileName);
+      } else {
+        reason = det['reason'];
+        setState(() {
+          trydown = true;
+        });
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,14 +106,15 @@ class _payslipState extends State<payslip> {
           Padding(
             padding: const EdgeInsets.all(15.0),
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Expanded(child: Text('Select Year ')),
                 DropdownButton(
+                  hint: Text("Select Year"),
                   //isExpanded: true, // here need to change
-                  value: dropdownValue,
-                  onChanged: (String? newValue) {
+                  value: year,
+                  onChanged: (newValue) {
                     setState(() {
-                      dropdownValue = newValue!;
+                      year = newValue!;
                     });
                   },
                   items: <String>[
@@ -62,23 +131,63 @@ class _payslipState extends State<payslip> {
                     );
                   }).toList(),
                 ),
+                DropdownButton(
+                  hint: Text("Select Month"),
+                  //isExpanded: true, // here need to change
+                  value: month,
+                  onChanged: (newValue) {
+                    setState(() {
+                      month = newValue!;
+                    });
+                  },
+                  items: <String>[
+                    '1',
+                    '2',
+                    '3',
+                    '4',
+                    '5',
+                    '6',
+                    '7',
+                    '8',
+                    '9',
+                    '10',
+                    '11',
+                    '12',
+                  ].map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 0, 50, 0),
+                        child: Text(value),
+                      ),
+                    );
+                  }).toList(),
+                ),
               ],
             ),
           ),
-          Expanded(
-            child: ListView.builder(
-              padding: const EdgeInsets.all(8),
-              itemCount: 3,
-              itemBuilder: (BuildContext context, int index) {
-                return Card(
-                  child: ListTile(
-                    leading: Text('${month[index]},$dropdownValue'),
-                    trailing: Icon(Icons.download),
-                  ),
-                );
-              },
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              onPrimary: Colors.black87,
+              minimumSize: Size(300, 60),
+              padding: EdgeInsets.all(25),
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(2)),
+              ),
+            ),
+            onPressed: year == null || month == null
+                ? null
+                : () {
+                    fetchAlbum(context);
+                  },
+            child: Text(
+              'Download Payslip',
+              style: TextStyle(
+                color: Colors.white,
+              ),
             ),
           ),
+          if (trydown) Text(reason),
         ],
       )),
     );
